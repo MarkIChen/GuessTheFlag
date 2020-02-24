782780a417c7848739cf383151462ccc44ced3ef
MarkStudio <markchen8024@gmail.com>
2020-02-24 21:51:01 +0800

    change background colors.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,7 +10,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+    
+        ZStack {
+            VStack(spacing: 0) {
+                Color(red:0.51, green:0.93, blue:0.93)
+                Color(red:0.00, green:0.81, blue:0.79)
+                Color(red:0.98, green:0.69, blue:0.63)
+                Color(red:0.88, green:0.44, blue:0.33)
+            }.edgesIgnoringSafeArea(.all)
+            
+            Text("Hello World").bold()
+        }
     }
 }
 
@@ -19,3 +29,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+

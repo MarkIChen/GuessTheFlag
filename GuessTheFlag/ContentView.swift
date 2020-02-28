220e65b6e6d07279525f7833ea5c8fbaf48916ea
MarkStudio <markchen8024@gmail.com>
2020-02-28 21:44:37 +0800

    add flag button.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,16 +9,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    
+    static let countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russian", "Spain", "UK", "US"]
+    
+    var correctAnswear = Int.random(in: 0...2)
     
     var body: some View {
-        Button("Clicked Me.") {
-            self.showingAlert = true
+        
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                VStack(spacing: 30) {
+                    Text("Tap the flag of").foregroundColor(.white)
+                    Text(ContentView.countries[correctAnswear]).foregroundColor(.white)
+                    
+                }
+                Divider()
+                FlagButtonList()
+                Spacer()
+            }
         }
-        .alert(isPresented: $showingAlert) {
-            Alert(title: Text("Hello SiftUI"), message: Text("some details."), dismissButton: .default(Text("OK")))
+        
+        
+    }
+}
+
+struct FlagButtonList: View {
+    var body: some View {
+        ForEach(0 ..< 3) { number in
+            Button(action: {
+                // button pressed
+                print(number)
+            }) {
+                Image(ContentView.countries[number]).renderingMode(.original)
+            }
         }
-     
     }
 }
 
